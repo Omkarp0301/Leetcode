fe6e16cec6579a76143a8241a1aa8aff093dcfc0
Omkarp0301 <139928587+Omkarp0301@users.noreply.github.com>
2023-11-04 02:27:48 +0530

    Time: 3 ms (95.68%), Space: 7.5 MB (83.37%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,41 @@
 class Solution {
+private:
+    bool checkEqual(int a[26], int b[26]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) {
-            return false;
+        int count1[26] = {0};
+        int count2[26] = {0};
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+
+        if (s1Len > s2Len) {
+            return false; // s1 cannot be a permutation of s2 if it's longer
         }
 
-        vector<int> s1_count(26, 0);
-        vector<int> s2_count(26, 0);
-
-        // Calculate the frequency of characters in s1 and the initial window in s2
-        for (int i = 0; i < s1.length(); i++) {
-            s1_count[s1[i] - 'a']++;
-            s2_count[s2[i] - 'a']++;
+        // Initialize count1 and count2 for the first s1Len characters in s2
+        for (int i = 0; i < s1Len; i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
         }
 
-        for (int i = s1.length(); i < s2.length(); i++) {
-            if (s1_count == s2_count) {
-                return true;
+        for (int i = s1Len; i < s2Len; i++) {
+            if (checkEqual(count1, count2)) {
+                return true; // Found a permutation
             }
 
-            // Update the character count for the new character entering the window
-            s2_count[s2[i] - 'a']++;
-
-            // Remove the count of the character leaving the window
-            s2_count[s2[i - s1.length()] - 'a']--;
+            // Update count2 for the sliding window
+            count2[s2[i] - 'a']++;
+            count2[s2[i - s1Len] - 'a']--;
         }
 
-        // Check the last window
-        return s1_count == s2_count;
+        return checkEqual(count1, count2); // Check for the last window
     }
 };
